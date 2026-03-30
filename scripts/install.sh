e6d570d524aa837852282ce7999754a3f2405b6e
bitrunner <mrflint46@gmail.com>
2026-03-30 22:15:26 +0200

    fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,20 +4,20 @@
 set -euo pipefail
 
 # Scripts
-mkdir -p ~/.local/bin && ln -sfn ~/.config/dotfiles/scripts/* ~/.local/bin
+mkdir -p ~/.local/bin && ln -sfn ~/.config/dotfiles_utilities/scripts/* ~/.local/bin
 
 # NeoVim
-ln -sfn ~/.config/dotfiles/nvim ~/.config/nvim
+ln -sfn ~/.config/dotfiles_utilities/nvim ~/.config/nvim
 
 # Vim
-ln -sfn ~/.config/dotfiles/.vimrc ~/.vimrc
+ln -sfn ~/.config/dotfiles_utilities/.vimrc ~/.vimrc
 
 # Tmux
-ln -sfn ~/.config/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sfn ~/.config/dotfiles_utilities/.tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Alacritty
-ln -sfn ~/.config/dotfiles/.alacritty.toml ~/.alacritty.toml
+ln -sfn ~/.config/dotfiles_utilities/.alacritty.toml ~/.alacritty.toml
 
 # Yazi
-ln -sfn ~/.config/dotfiles/yazi ~/.config/yazi
+ln -sfn ~/.config/dotfiles_utilities/yazi ~/.config/yazi
